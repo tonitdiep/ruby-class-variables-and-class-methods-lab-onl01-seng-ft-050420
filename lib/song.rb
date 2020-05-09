0d6afaf6ba8c6ff9a6ecd6917bece76ef8db1641
Toni  Diep <adiep123@gmail.com>
2020-05-09 22:23:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,10 @@
 class Song
-attr_accessor :name, :artist, :genre  
+attr_accessor :name, :artist, :genres  
 @@count = 0
 def initialize()
   @name = name
   @artist = artist
-  @genre = genre
+  @genres = genres
   @@count += 1
 end
 
