9128a1481782461fbb8618164187351813aed6b1
Toni  Diep <adiep123@gmail.com>
2020-05-09 23:33:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,10 +27,15 @@ end
 
   def self.genre_count
   genre_count = 0
-  @@genres.each {|genre| genre_count[genre] +=1}
-    return @@genre_count
+  @@genres.each do |genre|
+    if genre_count[genre]
+      genre_count[genre] += 1
+    else
+      genre_count[genre] = 1
+    end
   end
-
+  genre_count
+  end
 @@artists = [] 
 # def self.artist_count
 #   @artists = []  
